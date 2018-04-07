b0f6f5cdfa03dddb4b98023f256ff3dc7f65dcd8
Dynisious <daniel.bechaz@gmail.com>
2018-04-07 13:56:52 +1000

    Fixed It?

---
+++ src/bin/rocket/main.rs
@@ -0,0 +1,19 @@
+
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate UniHack;
+extern crate rocket;
+
+mod index;
+mod quiz;
+mod assets;
+mod results;
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index::index,assets::get_file,quiz::quiz_page,results::get_neural_results, results::return_results])
+}
+
+fn main() {
+    rocket().launch();
+}

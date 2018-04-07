8bbd0bda3ffd73f6bf2c401056e4f4c19068a3b5
Jonathan Nicholas <jonicrecis@gmail.com>
2018-04-07 12:40:43 +1000

    index page error check fix

--- src/bin/rocket/index.rs
+++ src/bin/rocket/index.rs
@@ -1,11 +1,8 @@
 use rocket::response::NamedFile;
-use rocket::response::content::Html;
 use std::path::Path;
 
 //Index.html
-//improper error handling for now
 #[get("/")]
-pub fn index() -> Html<NamedFile> {
-    let index_page = NamedFile::open(Path::new("statics/index.html")).expect("404");
-    Html(index_page)
+pub fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("statics/index.html")).ok()
 }

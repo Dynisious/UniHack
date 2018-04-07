cc2a4a00d5925157c6f43293a16d8cf243bde035
Dynisious <daniel.bechaz@gmail.com>
2018-04-07 12:29:30 +1000

    Corrected Return Value

--- src/bin/rocket/assets.rs
+++ src/bin/rocket/assets.rs
@@ -2,6 +2,6 @@ use rocket::response::NamedFile;
 use std::path::Path;
 
 #[get("/assets/<filename>")]
-pub fn get_file(filename: String) -> NamedFile {
-    NamedFile::open(Path::new("assets/").join(filename)).expect("404")
+pub fn get_file(filename: String) -> Option<NamedFile> {
+    NamedFile::open(Path::new("assets/").join(filename))
 }

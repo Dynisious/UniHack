f4eea4c13510f2a58f0c44374bb75eceab5db885
Jonathan Nicholas <jonicrecis@gmail.com>
2018-04-07 13:10:26 +1000

    results routing and neural network call

--- src/bin/rocket/main.rs
+++ src/bin/rocket/main.rs
@@ -7,9 +7,10 @@ extern crate rocket;
 mod index;
 mod quiz;
 mod assets;
+mod results;
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index::index,assets::get_file,quiz::quiz_page])
+    rocket::ignite().mount("/", routes![index::index,assets::get_file,quiz::quiz_page,results::get_neural_results, results::return_results])
 }
 
 fn main() {

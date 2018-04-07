21f328304008cf777f2d2149eeadc72fa8067ffc
Dynisious <daniel.bechaz@gmail.com>
2018-04-07 15:45:15 +1000

    Network Serialisation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
 mod neural_net;
 
+use std::fs::File;
 use neural_net::*;
+pub use neural_net::NeuralNet;
 
-pub fn neural_net(inputs: &[usize]) -> [usize; NEURAL_OUTPUT] {
+pub fn neural_net(inputs: &[usize], network: &NeuralNet) -> [usize; NEURAL_OUTPUT] {
     [0, 1, 2]
+}
+
+pub fn store_network(file: File, network: &NeuralNet) -> serde_json::Result<()> {
+    serde_json::to_writer(file, network)
+}
+
+pub fn load_network(file: File) -> serde_json::Result<NeuralNet>{
+    serde_json::from_reader(file)
 }
